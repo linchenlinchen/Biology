f6064b3b37045396d8fd783cb27918594ec1528f
caojuntao <18302010002@fudan.edu.cn>
2021-02-07 23:58:05 +0800

    square is ok

--- pages/individual/square.js
+++ pages/individual/square.js
@@ -1,18 +1,64 @@
 // pages/individual/square.js
+const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    projects:[
+      {
+      picture:"../../images/1.png",
+      title:"表型组-泰州序列",
+      time:"2000.2.2",
+      background:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+     },
+     {
+      picture:"../../images/1.png",
+      title:"表型组-泰州序列",
+      time:"2000.2.2",
+      background:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+     },
+     {
+      picture:"../../images/1.png",
+      title:"表型组-泰州序列",
+      time:"2000.2.2",
+      background:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+     },
+     {
+      picture:"../../images/1.png",
+      title:"表型组-泰州序列",
+      time:"2000.2.2",
+      background:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+     },
+    ],
+    focus:false,  //控制是否显示带取消按钮的搜索框
+    inputValue:"",
+    userInfo: {},
+    hasUserInfo: false,
 
+
+  },
+  focusHandler(e){
+    this.setData({focus:true});
+  },
+  cancelHandler(e)
+  {
+    this.setData({focus:false});
+    this.setData({inputValue:""});
+  },
+  query(e){
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
   },
 
   /**
