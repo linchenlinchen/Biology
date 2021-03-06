76d30b3354bad4774d96958c87722c7991fe6078
linchenlinchen <34669038+linchenlinchen@users.noreply.github.com>
2021-03-06 11:07:29 +0800

    add 8 jump pages

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,6 +10,14 @@ Page({
     programs:'进入我的项目',
     square:'进入项目广场',
     contents:'进入项目内容',
+    login:'登录',
+    volunteer_login:'志愿者登录',
+    agreement:'问卷填写知情同意页',
+    agreement_type:'问卷填写同意类型页',
+    apartment_login:'单位登录',
+    management:'问卷管理页',
+    query_result:'查询问卷结果页',
+    edit:'编辑或创建问卷结果页',
     exit: '退出小程序',
     userInfo: {},
     hasUserInfo: false,
@@ -44,6 +52,46 @@ Page({
   jump2Contents(){
     wx.navigateTo({
       url: '../individual/contents',
+    })
+  },
+  jump2Login(){
+    wx.navigateTo({
+      url: '../individual/login',
+    })
+  },
+  jump2VolunteerLogin(){
+    wx.navigateTo({
+      url: '../individual/volunteer_login',
+    })
+  },
+  jump2Agreement(){
+    wx.navigateTo({
+      url: '../individual/agreeement',
+    })
+  },
+  jump2AgreementType(){
+    wx.navigateTo({
+      url: '../individual/agreement_type',
+    })
+  },
+  jump2ApartmentLogin(){
+    wx.navigateTo({
+      url: '../individual/apartment_login',
+    })
+  },
+  jump2Management(){
+    wx.navigateTo({
+      url: '../individual/management',
+    })
+  },
+  jump2QueryResult(){
+    wx.navigateTo({
+      url: '../individual/query_result',
+    })
+  },
+  jump2Edit(){
+    wx.navigateTo({
+      url: '../individual/edit',
     })
   },
   onLoad() {
